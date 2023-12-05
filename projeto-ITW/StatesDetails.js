891d8acab86f953ed2dcf613037341ec28cb8d45
Oxx05 <bernardoam05@ua.pt>
2023-12-05 12:57:14 +0000

    a

---
+++ projeto-ITW/StatesDetails.js
@@ -0,0 +1,108 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/api/States/');
+    self.displayName = 'NBA States Details';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    //--- Data Record
+    self.Id = ko.observable('');
+    self.Name = ko.observable('');
+    self.StateId = ko.observable('');
+    self.StateName = ko.observable('');
+    self.AcronymId = ko.observable('');
+    self.ConferenceId = ko.observable('');
+    self.ConferenceName = ko.observable('');
+    self.City = ko.observable('');
+    self.DivisionName = ko.observable('');
+    self.DivisionId = ko.observable('');
+    self.Logo = ko.observable('');
+    self.History = ko.observable('');
+    //--- Page Events
+    self.activate = function (id) {
+        console.log('CALL: getStates...');
+        var composedUri = self.baseUri() + id;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            hideLoading();
+            self.Id(data.Id);
+            self.Name(data.Name);
+            self.Acronym(data.Acronym);
+            self.StateId(data.StateId);
+            self.StateName(data.StateName);
+            self.ConferenceId(data.ConferenceId);
+            self.ConferenceName(data.ConferenceName);
+            self.DivisionName(data.DivisionName);
+            self.DivisionId(data.DivisionId);
+            self.City(data.City);
+            self.History(data.History);
+            self.Logo(data.Logo);
+        });
+    };
+
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    function showLoading() {
+        $('#myModal').modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('id');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("document.ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
