a5dedad147a986a4f79174d13c30cd78495cbf4d
Oxx05 <bernardoam05@ua.pt>
2023-12-10 19:20:19 +0000

    a

--- favoriteArenas.js
+++ favoriteArenas.js
@@ -1,20 +1,18 @@
-﻿// ViewModel KnockOut
+// ViewModel KnockOut
 var vm = function () {
+    
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
+    
     self.searchb = ko.observable('');
-    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Arenas');
-    self.displayName = 'NBA Arenas List';
+    console.log("did it");
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/api/Arenas');
+    self.displayName = 'NBA Favorite Arenas List';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
     self.records = ko.observableArray([]);
-    self.currentPage = ko.observable(1);
-    self.pagesize = ko.observable(20);
-    self.totalRecords = ko.observable(50);
-    self.hasPrevious = ko.observable(false);
-    self.hasNext = ko.observable(false);
-    self.search = function() { 
+    self.search = function() {
         console.log("searching")
         if ($("#searchb").val() === "") {
             showLoading();
@@ -41,102 +39,37 @@ var vm = function () {
             hideLoading();
             for (var i in data) {
                 self.arenaslist.push(data[i]);
-                } 
+                }
             });
         };
     };
-    self.clean = function() { 
-        console.log("Clean")
-        $("#searchb").val('')
-            var Uri ='http://192.168.160.58/NBA/api/Arenas'
-            self.playerlist = [];
-            ajaxHelper(Uri, 'GET').done(function (data) {
-            console.log(data);
-            hideLoading();
-            self.records(data.Records);
-            self.currentPage(data.CurrentPage);
-            self.hasNext(data.HasNext);
-            self.hasPrevious(data.HasPrevious);
-            self.pagesize(20)
-            self.totalPages(data.TotalPages);
-            self.totalRecords(data.TotalRecords)
-            
-                //self.SetFavourites();
-            });
-        };
-        self.onEnter = function(d,e) {
-            e.keyCode === 13 && self.search();
-            return true;
-        }; 
-        self.favoriteArenas = function (id, event) {
-            console.log('favourite click!')
+    //--- Page Events
+    self.activate = function () {
+        console.log('CALL: getArenas...');
+        var composedUri = self.baseUri();
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            //console.log(data);
+            console.log(composedUri);
             if (JSON.parse(window.localStorage.getItem('favArenas0')) == null) {
-                console.log('no favArenas in local storage, lets create it');
-                window.localStorage.setItem('favArenas0', '[]');
-                var a = JSON.parse(window.localStorage.getItem('favArenas0'));
-                b = a.concat([id]);
-                console.log(b)
-                window.localStorage.setItem('favArenas0', JSON.stringify(b));
+                self.records(null)
             } else {
-                var c = JSON.parse(window.localStorage.getItem('favArenas0'))
-                for (var i = 0; i < c.length; i++) {
-                    if (id == c[i]) {
-                        return false
+                console.log('checking which Arenas were favourited')
+                var arenasList = [];
+                var favArenasList = JSON.parse(window.localStorage.getItem('favArenas0'));
+                var a = favArenasList.length;
+                console.log(favArenasList,data.TotalRecords)
+                for (var i = 0; i < 50; i++) {
+                    for (var k = 0; k < a; k++) {
+                        if (favArenasList[k] == data.Records[i].Id) {
+                            arenasList.push(data.Records[i])
+                        }
                     }
                 }
-                var a = JSON.parse(window.localStorage.getItem('favArenas0'));
-                b = a.concat([id]);
-                window.localStorage.setItem('favArenas0', JSON.stringify(b));
-                console.log('Arena not favourited, added to favourites')
+                self.records(arenasList)
             }
-            console.log(JSON.parse(window.localStorage.getItem('favArenas0')))
-        }   
-    self.previousPage = ko.computed(function () {
-        return self.currentPage() * 1 - 1;
-    }, self);
-    self.nextPage = ko.computed(function () {
-        return self.currentPage() * 1 + 1;
-    }, self);
-    self.fromRecord = ko.computed(function () {
-        return self.previousPage() * self.pagesize() + 1;
-    }, self);
-    self.toRecord = ko.computed(function () {
-        return Math.min(self.currentPage() * self.pagesize(), self.totalRecords());
-    }, self);
-    self.totalPages = ko.observable(0);
-    self.pageArray = function () {
-        var list = [];
-        var size = Math.min(self.totalPages(), 9);
-        var step;
-        if (size < 9 || self.currentPage() === 1)
-            step = 0;
-        else if (self.currentPage() >= self.totalPages() - 4)
-            step = self.totalPages() - 9;
-        else
-            step = Math.max(self.currentPage() - 5, 0);
-
-        for (var i = 1; i <= size; i++)
-            list.push(i + step);
-        return list;
-    };
-
-    //--- Page Events
-    self.activate = function (id) {
-        console.log('CALL: getArenas...');
-        var composedUri = self.baseUri() + "?page=" + id + "&pageSize=" + self.pagesize();
-        ajaxHelper(composedUri, 'GET').done(function (data) {
-            console.log(data);
             hideLoading();
-            self.records(data.Records);
-            self.currentPage(data.CurrentPage);
-            self.hasNext(data.HasNext);
-            self.hasPrevious(data.HasPrevious);
-            self.pagesize(data.PageSize)
-            self.totalPages(data.TotalPages);
-            self.totalRecords(data.TotalRecords);
-            
-            
         });
+        console.log(self.records())
     };
 
     //--- Internal functions
@@ -200,6 +133,7 @@ var vm = function () {
     console.log("VM initialized!");
     
 };
+
 $(document).ready(function () {
     console.log("ready!");
     ko.applyBindings(new vm());
@@ -234,9 +168,12 @@ $(document).ready(function () {
             results: function() {}
         }
     });
+    $("#clearFavourites").click(function() {
+        if (!(JSON.parse(window.localStorage.getItem('favArenas0')) == null)) {
+            window.localStorage.clear()
+            window.location.reload()
+        } else {
+            alert("No favourites to clear")
+        }
+    });
 });
-
-
-$(document).ajaxComplete(function (event, xhr, options) {
-    $("#myModal").modal('hide');
-})
