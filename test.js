cda896d30f51498c45876451725ffa8e48bffce2
sle3pyy <simaomarquesantos@gmail.com>
2023-12-16 22:25:17 +0000

    bingus

--- test.js
+++ test.js
@@ -2,8 +2,27 @@
 var vm= function(){
 	self.records=ko.observableArray([]);
 
+	$('#submit').on('click', function(){
+		var date = new Date($('#gameDate').val());
+		var day = date.getDate();
+		var month = date.getMonth() + 1;
+		var year = date.getFullYear();
+		console.log([day, month, year].join('-'));
+		const settings = {
+			async: true,
+			crossDomain: true,
+			url: 'https://free-nba.p.rapidapi.com/games?dates[]='+[day, month, year].join('-'),
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '333f3343bamshe77bbbb8722c2d0p1611a4jsnc97fdf9ec78c',
+				'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+			}
+		};
+		self.activate(settings);
+		});
 
-	self.activate = function () {
+
+	self.activate = function (settings) {
         console.log('CALL: Games...');
 		$.ajax(settings).done(function (response) {
 			self.records(response);
@@ -11,35 +30,28 @@ var vm= function(){
 		});
     };
 
-	function showLoading() {
-        $("#myModal").modal('show', {
-            backdrop: 'static',
-            keyboard: false
-        });
-    }
-    function hideLoading() {
-        $('#myModal').on('shown.bs.modal', function (e) {
-            $("#myModal").modal('hide');
-        })
-    }
-
-
-	showLoading();
-	self.activate();
+	function todaysGames(){
+		const date = new Date();
+		let day = date.getDate();
+		let month = date.getMonth() + 1;
+		let year = date.getFullYear();
+		let currentDate = `${day}-${month}-${year}`;
+		const settings = {
+			async: true,
+			crossDomain: true,
+			url: 'https://free-nba.p.rapidapi.com/games?dates[]='+currentDate,
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '333f3343bamshe77bbbb8722c2d0p1611a4jsnc97fdf9ec78c',
+				'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+			}
+		};
+		self.activate(settings);
+	}
+	todaysGames();
     console.log("VM initialized!");
 
 }
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://free-nba.p.rapidapi.com/games?dates[]=2023-12-17',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '333f3343bamshe77bbbb8722c2d0p1611a4jsnc97fdf9ec78c',
-		'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-	}
-};
-
 
 $(document).ready(function () {
     console.log("ready!");
