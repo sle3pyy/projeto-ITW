184dd0aa84276ecc16915c94b717aaa8346f0b07
sle3pyy <simaomarquesantos@gmail.com>
2023-12-16 21:59:47 +0000

    e

--- test.js
+++ test.js
@@ -1,14 +1,47 @@
+
+var vm= function(){
+	self.records=ko.observableArray([]);
+
+
+	self.activate = function () {
+        console.log('CALL: Games...');
+		$.ajax(settings).done(function (response) {
+			self.records(response);
+			console.log(self.records());
+		});
+    };
+
+	function showLoading() {
+        $("#myModal").modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+
+	showLoading();
+	self.activate();
+    console.log("VM initialized!");
+
+}
 const settings = {
 	async: true,
 	crossDomain: true,
-	url: 'https://api-nba-v1.p.rapidapi.com/seasons',
+	url: 'https://free-nba.p.rapidapi.com/games?dates[]=2023-12-17',
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '333f3343bamshe77bbbb8722c2d0p1611a4jsnc97fdf9ec78c',
-		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+		'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
 });
