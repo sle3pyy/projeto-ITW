478b909e4726dbfa2320d1271cf807e4491dcb78
Oxx05 <bernardoam05@ua.pt>
2023-12-16 17:45:32 +0000

    a

---
+++ arenaMaps.js
@@ -0,0 +1,25 @@
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    console.log("sPageURL=", sPageURL);
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+//--- start ....
+showLoading();
+var pg = getUrlParameter('page');
+console.log(pg);
+if (pg == undefined)
+    self.activate(1);
+else {
+    self.activate(pg);
+}
+console.log("VM initialized!");
