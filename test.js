3997a1e7bd6151aa2320396fb23f2763172c1a0b
sle3pyy <simaomarquesantos@gmail.com>
2023-12-16 23:05:39 +0000

    e

--- test.js
+++ test.js
@@ -34,7 +34,7 @@ var vm= function(){
 
 	function todaysGames(){
 		const date = new Date();
-		let day = date.getDate();
+		let day = date.getDate()-1;
 		let month = date.getMonth() + 1;
 		let year = date.getFullYear();
 		let currentDate = `${day}-${month}-${year}`;
