5ec260e6f1c479bf4228b371e22e01a1e848c8ca
Oxx05 <bernardoam05@ua.pt>
2023-12-16 23:07:11 +0000

    a

--- arenaDetails.js
+++ arenaDetails.js
@@ -3,7 +3,7 @@ var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Arenas/3');
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Arenas/');
     self.displayName = 'NBA Arena Details';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
