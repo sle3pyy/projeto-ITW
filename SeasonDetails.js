119d095136d895da88201aab3613500d5d6b67ff
sle3pyy <simaomarquesantos@gmail.com>
2023-12-18 11:25:23 +0000

    e

--- SeasonDetails.js
+++ SeasonDetails.js
@@ -95,3 +95,13 @@ $(document).ready(function () {
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
 })
+document.querySelectorAll('[data-bs-toggle="collapse"]').forEach(function (element) {
+    element.addEventListener('click', function () {
+        var arrow = this.querySelector('.arrow');
+        if (arrow.textContent === '▶') {
+            arrow.textContent = '▼';
+        } else {
+            arrow.textContent = '▶';
+        }
+    });
+});
