e6e3ab9f005d2f4fd8b02c86fb7ca70e0f977ca8
sle3pyy <simaomarquesantos@gmail.com>
2024-01-01 18:28:08 +0000

    dark-mode final

--- darkModeToggler.js
+++ darkModeToggler.js
@@ -5,32 +5,27 @@ function toggler(){
         document.getElementById('dark-mode-toggle').innerHTML=('<i class="fa fa-sun-o"></i>');
         document.body.classList.remove('dark-mode');
         localStorage.setItem('color-mode','light');
-        console.log(localStorage.getItem('color-mode'));
     }
     else {
         document.documentElement.setAttribute('data-bs-theme','dark');
         document.body.classList.add('dark-mode');
         document.getElementById('dark-mode-toggle').innerHTML=('<i class="fa fa-moon-o"></i>');
         localStorage.setItem('color-mode','dark');
-        console.log(localStorage.getItem('color-mode'));
     } 
 }
 function starter(){
     if (localStorage.getItem('color-mode') == null) {
         localStorage.setItem('color-mode','light');
-        console.log(localStorage.getItem('color-mode'));
     }
     else if (localStorage.getItem('color-mode') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme','dark');
         document.body.classList.add('dark-mode');
         document.getElementById('dark-mode-toggle').innerHTML=('<i class="fa fa-moon-o"></i>');
-        console.log(localStorage.getItem('color-mode'));
     }
     else {
         document.documentElement.setAttribute('data-bs-theme','light');
         document.getElementById('dark-mode-toggle').innerHTML=('<i class="fa fa-sun-o"></i>');
         document.body.classList.remove('dark-mode');
-        console.log(localStorage.getItem('color-mode'));
     }
 
 }
